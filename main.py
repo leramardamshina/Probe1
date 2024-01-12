3c37ae9c069c2acab910724b153ef9b634a34b36
Валерия <leramardamshina@yandex.ru>
2024-01-12 23:37:17 +0500

    Модуль и пакеты main.py

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-def test():
-    a, b = 1, 2
-    print('simple:', a, b)
-test()
 
+import name
 
-def test2 (x, y, z):
-    return (x ** 2 + y ** 2 + z ** 2) ** 3
-res = test2 (2,4,6)
-print(res)
+print(name.func_1)
