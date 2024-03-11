542cedf6d0295cf378cb4d278d7f7c5b625a3b55
Валерия <leramardamshina@yandex.ru>
2024-03-12 01:01:17 +0500

    Домашнее задание по теме "Потоки на классах"

--- Потоки на классах.py
+++ Потоки на классах.py
@@ -6,36 +6,28 @@
 #Враги будут нападать в количестве 100 человек. Каждый день рыцарь может ослабить вражеское войско на skill-человек.
 #Если у рыцаря skill равен 20, то защищать крепость он будет 5 дней (5 секунд в программе).
 #Чем выше умение, тем быстрее рыцарь защитит королевство.
-#enemy_count - кол-во врагов
-#defend_per_day - каждый день убывает столько-то количество врагов
-#remaining_enemies - оставшиеся враги
 
-import random
 import time
 from threading import Thread
 
 class Knight(Thread):
     def __init__(self, name, skill, *args, **kwargs):
-        super(Knight, self).__init__(*args, **kwargs)  #супер используется вместо родительского класса
+        super(Knight, self).__init__(*args, **kwargs)
         self.name = name
         self.skill = skill
 
     def run(self):
-        enemy_count = random.randint(1, 100) #кол-во врагов
-        print(f'Враги нападают в количестве {enemy_count} человек', flush=True)
-        if self.skill >= 20:
-            defend_per_day = self.skill
-            remaining_enemies = enemy_count - defend_per_day
-            print(f'Рыцарь {self.name} ослабил вражевское войско на {remaining_enemies} человек')
-            if remaining_enemies >= 0:
-                print(f'Рыцарь {self.name} победил')
-            else:
-                print(f'Продолжаем сражаться, врагов осталось {remaining_enemies}', flush=True)
-                time.sleep(5)
-        else:
-            print(f'Продолжаем сражаться')
-
-
+        print(f'{self.name} на нас напали!', flush=True)
+        enemies = 100
+        days = 0
+        while enemies > 0:
+            days += 1
+            enemies = enemies - self.skill
+            if enemies < 0:
+                enemies = 0
+            print(f' {self.name} сражается {days} день(дня)... , осталось {enemies} воинов', flush=True)
+            time.sleep(5)
+        print(f'{self.name} одержал побежу спустя {days} дней',flush=True)
 
 knight1 = Knight("Sir Lancelot", 10) # Низкий уровень умения
 knight2 = Knight("Sir Galahad", 20) # Высокий уровень умения
@@ -45,3 +37,5 @@ knight2.start()
 
 knight1.join()
 knight2.join()
+
+print("Все битвы закончились!")
